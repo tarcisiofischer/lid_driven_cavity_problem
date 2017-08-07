81022f00621364e1b1f4f2fc9e316c319d414d17
Tarcisio Fischer <tarcisio@esss.com.br>
2017-08-07 18:33:16 -0300

    WIP: Set U, V and P in the residual_function, so that we have a simple way to define the increments for the newton solver.
    Also, move all function implementation to outside the graph (Was not implemented anyway), because it seems more explicit this way

--- lid_driven_cavity_problem/residual_function.py
+++ lid_driven_cavity_problem/residual_function.py
@@ -1,10 +1,9 @@
-def residual_function(graph):
+def residual_function(U, V, P, graph):
     # Notes:
     #
     # Think about using petsc4py's DMDA so that we don't need to worry about
     # the Jacobian matrix.
     #
-
     unknowns = 3
     residual = [None] * len(graph) * unknowns
     mass_equation_offset = len(graph) * 0
@@ -13,90 +12,79 @@ def residual_function(graph):
 
     dt = graph.dt
 
-    # Conservation of Mass
-    # u_e * dy - u_w * dy + v_n * dx - v_s * dx
-    start = mass_equation_offset * len(graph)
-    for i, node in range(start, start + len(graph)):
-        dx = node.dx
-        dy = node.dy
-        u_e = node.u_e
-        u_w = node.u_w
-        v_n = node.v_n
-        v_s = node.v_s
-  
-        residual[i] = (u_e * dy - u_w * dy) + (v_n * dx - v_s * dx)
- 
-    # Navier Stokes X 
-    start = ns_x_equation_offset * len(graph)
-    for i, node in range(start, start + len(graph)):
+    for i, node in range(len(graph)):
+        # Constants
         dx = node.dx
         dy = node.dy
         rho = node.rho
-        u_P = node.u_P
-        u_P_old = node.u_P_old
-        u_E = node.u_E
-        u_W = node.u_W
-        u_N = node.u_N
-        u_S = node.u_S
-        u_e = node.u_e
-        u_w = node.u_w
-        v_n = node.v_n
-        v_s = node.v_s
         mi = node.mi
+        U_P_old = node.u_P_old
+        V_P_old = node.v_P_old
+
+        # Unknowns
+        U_P = node.U_P_from(U)
+        U_E = node.U_E_from(U)
+        U_W = node.U_W_from(U)
+        U_N = node.U_N_from(U)
+        U_S = node.U_S_from(U)
+        V_P = node.V_P_from(V)
+        V_E = node.V_E_from(V)
+        V_SE = node.V_SE_from(V)
+        V_W = node.V_W_from(V)
+        V_N = node.V_N_from(V)
+        V_S = node.V_S_from(V)
+        P_P = node.P_P_from(P)
+        P_E = node.P_E_from(P)
+        P_W = node.P_W_from(P)
+
+        # Calculated
+        u_e = (U_E + U_P) / 2.0
+        u_w = (U_P + U_W) / 2.0
+        v_n = (V_E + V_P) / 2.0
+        v_s = (V_S + V_SE) / 2.0
         beta_u_e = node.beta_u_e
         beta_u_w = node.beta_u_w
         beta_v_n = node.beta_v_n
         beta_v_s = node.beta_v_s
         P_e = node.P_e
         P_w = node.P_w
+        du_e_dx = (U_E - U_P) / dx
+        du_w_dx = (U_P - U_W) / dx
+        du_n_dx = (U_N - U_P) / dy
+        du_s_dx = (U_P - U_S) / dy
+        dv_e_dx = (V_E - V_P) / dx
+        dv_w_dx = (V_P - V_W) / dx
+        dv_n_dx = (V_N - V_P) / dy
+        dv_s_dx = (V_P - V_S) / dy
 
-        transient_term = (rho * u_P - rho * u_P_old) * (dx * dy / dt)
+        # Conservation of Mass
+        ii = mass_equation_offset * len(graph) + i
+        residual[ii] = (u_e * dy - u_w * dy) + (v_n * dx - v_s * dx)
+ 
+        # Navier Stokes X 
+        transient_term = (rho * U_P - rho * U_P_old) * (dx * dy / dt)
         advective_term = \
-            rho * u_e * ((.5 - beta_u_e) * u_E + (.5 + beta_u_e) * u_P) * dy - \
-            rho * u_w * ((.5 - beta_u_w) * u_P + (.5 + beta_u_w) * u_W) * dy + \
-            rho * v_n * ((.5 - beta_v_n) * u_N + (.5 + beta_v_n) * u_P) * dx - \
-            rho * v_s * ((.5 - beta_v_s) * u_P + (.5 + beta_v_s) * u_S) * dx
+            rho * u_e * ((.5 - beta_u_e) * U_E + (.5 + beta_u_e) * U_P) * dy - \
+            rho * u_w * ((.5 - beta_u_w) * U_P + (.5 + beta_u_w) * U_W) * dy + \
+            rho * v_n * ((.5 - beta_v_n) * U_N + (.5 + beta_v_n) * U_P) * dx - \
+            rho * v_s * ((.5 - beta_v_s) * U_P + (.5 + beta_v_s) * U_S) * dx
         difusive_term  = \
-            mi * (u_E - u_P) / dx * dy - \
-            mi * (u_P - u_W) / dx * dy + \
-            mi * (u_N - u_P) / dy * dx - \
-            mi * (u_P - u_S) / dy * dx
+            mi * du_e_dx * dy - \
+            mi * du_w_dx * dy + \
+            mi * du_n_dx * dx - \
+            mi * du_s_dx * dx
         source_term    = -(P_e - P_w) * dy 
 
+        ii = ns_x_equation_offset * len(graph) + i
         residual[i] = transient_term + advective_term - difusive_term - source_term
 
-    # Navier Stokes Y
-    start = ns_y_equation_offset * len(graph)
-    for i, node in range(start, start + len(graph)):
-        dx = node.dx
-        dy = node.dy
-        rho = node.rho
-        v_P = node.u_P
-        v_P_old = node.u_P_old
-        v_E = node.u_E
-        v_W = node.u_W
-        v_N = node.u_N
-        v_S = node.u_S
-        u_e = node.u_e
-        u_w = node.u_w
-        v_n = node.v_n
-        v_s = node.v_s
-        mi = node.mi
-        beta_u_e = node.beta_u_e
-        beta_u_w = node.beta_u_w
-        beta_v_n = node.beta_v_n
-        beta_v_s = node.beta_v_s
-        dv_e_dx = node.dv_e_dx # (v_E - v_P) / dx
-        dv_w_dx = node.dv_w_dx # (v_P - v_W) / dx
-        dv_n_dx = node.dv_n_dx # (v_N - v_P) / dy
-        dv_s_dx = node.dv_s_dx # (v_P - v_S) / dy
-
-        transient_term = (rho * v_P - rho * v_P_old) * (dx * dy / dt)
+        # Navier Stokes Y
+        transient_term = (rho * V_P - rho * V_P_old) * (dx * dy / dt)
         advective_term = \
-            rho * u_e * ((.5 - beta_u_e) * v_E + (.5 + beta_u_e) * v_P) * dy - \
-            rho * u_w * ((.5 - beta_u_w) * v_P + (.5 + beta_u_w) * v_W) * dy + \
-            rho * v_n * ((.5 - beta_v_n) * v_N + (.5 + beta_v_n) * v_P) * dx - \
-            rho * v_s * ((.5 - beta_v_s) * v_P + (.5 + beta_v_s) * v_S) * dx
+            rho * u_e * ((.5 - beta_u_e) * V_E + (.5 + beta_u_e) * V_P) * dy - \
+            rho * u_w * ((.5 - beta_u_w) * V_P + (.5 + beta_u_w) * V_W) * dy + \
+            rho * v_n * ((.5 - beta_v_n) * V_N + (.5 + beta_v_n) * V_P) * dx - \
+            rho * v_s * ((.5 - beta_v_s) * V_P + (.5 + beta_v_s) * V_S) * dx
         difusive_term  = \
             mi * dv_e_dx * dy - \
             mi * dv_w_dx * dy + \
@@ -104,7 +92,8 @@ def residual_function(graph):
             mi * dv_s_dx * dx
         source_term    = -(node.P_n - node.P_s) * dy 
 
-        residual[i] = transient_term + advective_term - difusive_term - source_term
+        ii = ns_y_equation_offset * len(graph) + i
+        residual[ii] = transient_term + advective_term - difusive_term - source_term
 
     # Sanity check
     assert None not in residual, 'Missing equation in residual function'
