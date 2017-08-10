03fb044d7860b7e366f2eaf530a28fbad41d162a
Tarcisio Fischer <tarcisio@esss.com.br>
2017-08-10 09:53:00 -0300

    WIP: Fix indexes for staggered grids

--- lid_driven_cavity_problem/residual_function.py
+++ lid_driven_cavity_problem/residual_function.py
@@ -10,38 +10,58 @@ def residual_function(U, V, P, graph):
     ns_x_equation_offset = len(graph) * 1
     ns_y_equation_offset = len(graph) * 2
 
+    # Staggered grids
+    pressure_mesh = graph.pressure_mesh
+    ns_x_mesh = graph.ns_x_mesh
+    ns_y_mesh = graph.ns_y_mesh
+
+    # Knowns (Constants)
     dt = graph.dt
     dx = graph.dx
     dy = graph.dy
     rho = graph.rho
     mi = graph.mi
-    pressure_mesh = graph.pressure_mesh
-    ns_x_mesh = graph.ns_x_mesh
-    ns_y_mesh = graph.ns_y_mesh
 
     for i, pressure_node in range(len(pressure_mesh)):
-        # Knowns
-        U_e = pressure_node.U_e
-        U_w = pressure_node.U_w
-        V_n = pressure_node.V_n
-        V_s = pressure_node.V_s
+        # Index conversion
+        i_U_w = i - (i // pressure_mesh.nx) - 1
+        i_U_e = i_U_w + 1
+        i_V_n = i
+        i_V_s = i - (i // pressure_mesh.nx) * pressure_mesh.nx
+
+        # Unknowns
+        U_w = U[i_U_w]
+        U_e = U[i_U_e]
+        V_n = V[i_V_n]
+        V_s = V[i_V_s]
 
         # Conservation of Mass
         ii = mass_equation_offset * len(graph) + i
         residual[ii] = (U_e * dy - U_w * dy) + (V_n * dx - V_s * dx)
 
     for i, ns_x_node in range(len(ns_x_mesh)):
+        # Index conversion
+        i_U_P = i
+        i_U_W = i - 1
+        i_U_E = i + 1
+        i_U_N = i + ns_x_mesh.nx
+        i_U_S = i - ns_x_mesh.nx
+        i_P_w = i + (i // ns_x_mesh.nx)
+        i_P_e = i_P_w + 1
+
         # Knowns
         U_P_old = ns_x_node.U_P_old
-        U_P = ns_x_node.U_P
-        U_E = ns_x_node.U_E
-        U_W = ns_x_node.U_W
-        U_N = ns_x_node.U_N
-        U_S = ns_x_node.U_S
-        P_e = ns_x_node.P_e
-        P_w = ns_x_node.P_w
 
-        # Calculated
+        # Unknowns
+        U_P = U[i_U_P]
+        U_W = U[i_U_W]
+        U_E = U[i_U_E]
+        U_N = U[i_U_N]
+        U_S = U[i_U_S]
+        P_w = P[i_P_w]
+        P_e = P[i_P_e]
+
+        # Calculated (Interpolated and Secondary Variables)
         dU_e_dx = (U_E - U_P) / dx
         dU_w_dx = (U_P - U_W) / dx
         dU_n_dx = (U_N - U_P) / dy
@@ -73,17 +93,28 @@ def residual_function(U, V, P, graph):
         residual[i] = transient_term + advective_term - difusive_term - source_term
 
     for i, ns_y_node in range(len(ns_y_mesh)):
+        # Index conversion
+        i_V_P = i
+        i_V_W = i - 1
+        i_V_E = i + 1
+        i_V_N = i + ns_y_mesh.nx
+        i_V_S = i - ns_y_mesh.nx
+        i_P_n = i + ns_y_mesh.nx
+        i_P_s = i
+
         # Knowns
         V_P_old = ns_y_node.V_P_old
-        V_P = ns_y_node.V_P
-        V_E = ns_y_node.V_E
-        V_W = ns_y_node.V_W
-        V_N = ns_y_node.V_N
-        V_S = ns_y_node.V_S
-        P_n = ns_y_node.P_n
-        P_s = ns_y_node.P_s
 
-        # Calculated
+        # Unknowns
+        V_P = V[i_V_P]
+        V_E = V[i_V_E]
+        V_W = V[i_V_W]
+        V_N = V[i_V_N]
+        V_S = V[i_V_S]
+        P_n = P[i_P_n]
+        P_s = P[i_P_s]
+
+        # Calculated (Interpolated and Secondary Variables)
         beta_U_e = ns_y_node.beta_U_e
         beta_U_w = ns_y_node.beta_U_w
         beta_V_n = ns_y_node.beta_V_n
