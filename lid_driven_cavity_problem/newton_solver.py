0233e68ba901dcb4fc3112ac116caacae95e0eb8
Tarcisio Fischer <tarcisio@esss.com.br>
2017-08-17 18:14:38 -0300

    Extract common code to outside solver function

--- lid_driven_cavity_problem/newton_solver.py
+++ lid_driven_cavity_problem/newton_solver.py
@@ -12,6 +12,42 @@ IGNORE_DIVERGED = False
 class SolverDivergedException(RuntimeError):
     pass
 
+
+def _create_X(U, V, P):
+    if SOLVE_WITH_CLOSE_UVP:
+        X = U + V + P
+    else:
+        X = np.zeros(shape=(3 * len(P),))
+        X[0::3] = np.r_[U, np.ones(shape=(len(P) - len(U),))]
+        X[1::3] = np.r_[V, np.ones(shape=(len(P) - len(V),))]
+        X[2::3] = P
+    return X
+
+
+def _recover_X(X, graph):
+    pressure_mesh = graph.pressure_mesh
+    ns_x_mesh = graph.ns_x_mesh
+    ns_y_mesh = graph.ns_y_mesh
+
+    if SOLVE_WITH_CLOSE_UVP:
+        U = X[0:len(ns_x_mesh)]
+        V = X[len(ns_x_mesh):len(ns_x_mesh) + len(ns_y_mesh)]
+        P = X[len(ns_x_mesh) + len(ns_y_mesh):len(ns_x_mesh) + len(ns_y_mesh) + len(pressure_mesh)]
+    else:
+        U = X[0::3][0:len(ns_x_mesh)]
+        V = X[1::3][0:len(ns_y_mesh)]
+        P = X[2::3][0:len(pressure_mesh)]
+    return U, V, P
+
+
+def _plot_jacobian(graph, X):
+    import matplotlib.pyplot as plt
+    J = approx_jacobian(X, residual_function, 1e-4, graph)
+    J = J.astype(dtype='bool')
+    plt.imshow(J)
+    plt.show()
+
+
 def solve(graph):
     pressure_mesh = graph.pressure_mesh
     ns_x_mesh = graph.ns_x_mesh
@@ -20,21 +56,10 @@ def solve(graph):
     U = ns_x_mesh.phi
     V = ns_x_mesh.phi
     P = pressure_mesh.phi
-
-    if SOLVE_WITH_CLOSE_UVP:
-        X = U + V + P
-    else:
-        X = np.zeros(shape=(3 * len(P),))
-        X[0::3] = np.r_[U, np.zeros(shape=(len(P) - len(U),))]
-        X[1::3] = np.r_[V, np.zeros(shape=(len(P) - len(V),))]
-        X[2::3] = P
+    X = _create_X(U, V, P)
 
     if PLOT_JACOBIAN:
-        import matplotlib.pyplot as plt
-        J = approx_jacobian(X, residual_function, 1e-4, graph)
-        J = J.astype(dtype='bool')
-        plt.imshow(J)
-        plt.show()
+        _plot_jacobian(graph, X)
 
     X_, infodict, ier, mesg = fsolve(residual_function, X, args=(graph,), full_output=True)
     if SHOW_SOLVER_DETAILS:
