ab9a85c84b33eb3d525634549824d9e97bab33ad
Tarcisio Fischer <tarcisio@esss.com.br>
2017-08-18 18:56:24 -0300

    First version of jacobian non-zero structure computation with raw python code - Further improvements may be applied.

--- lid_driven_cavity_problem/newton_solver.py
+++ lid_driven_cavity_problem/newton_solver.py
@@ -55,6 +55,8 @@ def _recover_X(X, graph):
 
 __j_structure_cache = None
 def _calculate_jacobian_mask(N, graph):
+    COMPARE_JACOBIAN_AND_SHOW = False
+    
     global __j_structure_cache
     if __j_structure_cache is not None:
         return __j_structure_cache
@@ -64,21 +66,44 @@ def _calculate_jacobian_mask(N, graph):
     ns_y_mesh = graph.ns_y_mesh
     
     # Must have values in diagonal because of ILU preconditioner
-    j_structure = np.zeros((N, N), dtype=bool)
+    j_structure__new = np.zeros((N // 3, N // 3), dtype=bool)
 
-    for i in range(N):
-        j_structure[i,i] = 1.0
+    for i in range(N // 3):
+        j_structure__new[i, i] = 1.0
+        if i - 1 >= 0:
+            j_structure__new[i, i - 1] = 1.0
+        if i + 1 < N // 3:
+            j_structure__new[i, i + 1] = 1.0
+        if i - pressure_mesh.nx >= 0:
+            j_structure__new[i, i - pressure_mesh.nx] = 1.0
+        if i + pressure_mesh.nx < N // 3:
+            j_structure__new[i, i + pressure_mesh.nx] = 1.0
+        if i - pressure_mesh.nx + 1 >= 0:
+            j_structure__new[i, i - pressure_mesh.nx + 1] = 1.0
+        if i + pressure_mesh.nx - 1 < N // 3:
+            j_structure__new[i, i + pressure_mesh.nx - 1] = 1.0
 
-    # Creates the Jacobian 4 times, because of upwind asymmetries
-    for u_sign in [-1.0, 1.0]:
-        for v_sign in [-1.0, 1.0]:
-            fake_U = [u_sign * 1e-2 * (i + 1) ** 2 for i in range(len(ns_x_mesh.phi))]
-            fake_V = [v_sign * 1e-2 * (i + 1) ** 2 for i in range(len(ns_y_mesh.phi))]
-            fake_P = [1e3 * (i + 1) ** 2 for i in range(len(pressure_mesh.phi))]
-            fake_X = _create_X(fake_U, fake_V, fake_P, graph)
-            current_j_structure = approx_jacobian(fake_X, residual_function, 1e-4, graph).astype(dtype='bool')
-            j_structure = np.bitwise_or(j_structure, current_j_structure)
-    
+    j_structure__new = np.kron(j_structure__new, np.ones((3,3)))
+
+    if COMPARE_JACOBIAN_AND_SHOW:
+        j_structure = np.zeros((N, N), dtype=bool)
+        # Creates the Jacobian 4 times, because of upwind asymmetries
+        for u_sign in [-1.0, 1.0]:
+            for v_sign in [-1.0, 1.0]:
+                fake_U = [u_sign * 1e-2 * (i + 1) ** 2 for i in range(len(ns_x_mesh.phi))]
+                fake_V = [v_sign * 1e-2 * (i + 1) ** 2 for i in range(len(ns_y_mesh.phi))]
+                fake_P = [1e3 * (i + 1) ** 2 for i in range(len(pressure_mesh.phi))]
+                fake_X = _create_X(fake_U, fake_V, fake_P, graph)
+                current_j_structure = approx_jacobian(fake_X, residual_function, 1e-4, graph).astype(dtype='bool')
+                j_structure = np.bitwise_or(j_structure, current_j_structure)
+        
+        cmp = j_structure + j_structure__new
+        import matplotlib.pyplot as plt
+        plt.imshow(cmp, aspect='equal', interpolation='none')
+        plt.show()
+
+    j_structure = j_structure__new
+
     __j_structure_cache = j_structure
     return j_structure
 
