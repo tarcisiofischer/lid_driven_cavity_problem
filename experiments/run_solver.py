6cedaa575bae4969fb9524c253d660b8981c0b23
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-08-22 11:13:58 -0300

    Separate experiments and automated test cases

---
+++ experiments/run_solver.py
@@ -0,0 +1,59 @@
+from lid_driven_cavity_problem.newton_solver import solve_using_petsc, solve
+from lid_driven_cavity_problem.options import SOLVER
+from lid_driven_cavity_problem.staggered_grid import Graph
+from lid_driven_cavity_problem.time_stepper import run_simulation
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+PLOT_RESULTS = True
+
+print("-")
+print("TEST_RUN_SOLVER")
+
+size_x = 1.0
+size_y = 1.0
+nx = 30
+ny = 30
+dt = 1e-2
+rho = 1.0
+final_time = 0.1
+mi = 1.0
+Re = 100
+U_bc = (mi * Re) / (rho * size_x)
+print("Run Parameters:")
+print("size_x = %s" % (size_x,))
+print("size_y = %s" % (size_y,))
+print("nx = %s" % (nx,))
+print("ny = %s" % (ny,))
+print("dt = %s" % (dt,))
+print("rho = %s" % (rho,))
+print("mi = %s" % (mi,))
+print("U_bc = %s" % (U_bc,))
+print("Re = %s" % (Re,))
+print("")
+
+graph = Graph(size_x, size_y, nx, ny, dt, rho, mi, U_bc)
+if SOLVER == 'petsc':
+    solver = solve_using_petsc
+elif SOLVER == 'scipy':
+    solver = solve
+result = run_simulation(graph, final_time, solver)
+
+U = np.array(result.ns_x_mesh.phi)
+V = np.array(result.ns_y_mesh.phi)
+
+U = U.reshape(nx, ny - 1)
+V = V.reshape(nx - 1, ny)
+
+U = np.c_[[0.0] * nx, U, [0.0] * ny]
+U = (U[:, 1:] + U[:, :-1]) / 2.0
+
+V = np.r_[[[0.0] * nx], V, [[0.0] * ny]]
+V = (V[1:, :] + V[:-1, :]) / 2.0
+
+X, Y = np.meshgrid(np.arange(0.0, size_x, size_x / nx), np.arange(0.0, size_y, size_y / ny))
+plt.figure()
+plt.title("U and V Interpolated on the center of Pressure control volumes")
+plt.quiver(X, Y, U, V)
+plt.show()
---
+++ tests/test_run_solver.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from lid_driven_cavity_problem.newton_solver import solve_using_petsc
+from lid_driven_cavity_problem.staggered_grid import Graph
+from lid_driven_cavity_problem.time_stepper import run_simulation
+import numpy as np
+
+
+def test_small_case():
+    size_x = 1.0
+    size_y = 1.0
+    nx = 11
+    ny = 11
+    dt = 1e-2
+    rho = 1.0
+    final_time = 0.1
+    mi = 1.0
+    Re = 100
+    U_bc = (mi * Re) / (rho * size_x)
+
+    graph = Graph(size_x, size_y, nx, ny, dt, rho, mi, U_bc)
+    solver = solve_using_petsc
+    result = run_simulation(graph, final_time, solver)
+
+    U = np.array(result.ns_x_mesh.phi)
+    V = np.array(result.ns_y_mesh.phi)
+
+    expected_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_run_solver')
+    expected_U = np.loadtxt(os.path.join(expected_path, 'U.txt'))
+    expected_V = np.loadtxt(os.path.join(expected_path, 'V.txt'))
+
+    assert np.allclose(U, expected_U, rtol=1e-4, atol=1e-6)
+    assert np.allclose(V, expected_V, rtol=1e-4, atol=1e-6)
