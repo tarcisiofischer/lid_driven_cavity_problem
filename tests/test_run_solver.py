b243c272b4cc241e20cf245726bedf12300ba0c9
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-08-24 14:35:34 -0300

    Update test cases for nonlinear solvers

--- tests/test_run_solver.py
+++ tests/test_run_solver.py
@@ -7,6 +7,7 @@ from lid_driven_cavity_problem.staggered_grid import Graph
 from lid_driven_cavity_problem.time_stepper import run_simulation
 import numpy as np
 
+GENERATE = False
 
 @pytest.mark.parametrize(
     ("solver_function"),
@@ -18,13 +19,13 @@ import numpy as np
 def test_small_case(solver_function):
     size_x = 1.0
     size_y = 1.0
-    nx = 11
-    ny = 11
-    dt = 1e-2
+    nx = 15
+    ny = 15
+    dt = 0.05
     rho = 1.0
     final_time = 0.1
     mi = 1.0
-    Re = 100
+    Re = 10
     U_bc = (mi * Re) / (rho * size_x)
 
     graph = Graph(size_x, size_y, nx, ny, dt, rho, mi, U_bc)
@@ -34,8 +35,15 @@ def test_small_case(solver_function):
     V = np.array(result.ns_y_mesh.phi)
 
     expected_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_run_solver')
-    expected_U = np.loadtxt(os.path.join(expected_path, 'U.txt'))
-    expected_V = np.loadtxt(os.path.join(expected_path, 'V.txt'))
+    expected_U_path = os.path.join(expected_path, 'U.txt')
+    expected_V_path = os.path.join(expected_path, 'V.txt')
+    if GENERATE:
+        np.savetxt(expected_U_path, U)
+        np.savetxt(expected_V_path, V)
+        assert False, "Generation finished. Failing test (This is expected behavior)"
 
-    assert np.allclose(U, expected_U, rtol=1e-4, atol=1e-6)
-    assert np.allclose(V, expected_V, rtol=1e-4, atol=1e-6)
+    expected_U = np.loadtxt(expected_U_path)
+    expected_V = np.loadtxt(expected_V_path)
+
+    assert np.allclose(U, expected_U, rtol=1e-4, atol=1e-1)
+    assert np.allclose(V, expected_V, rtol=1e-4, atol=1e-1)
