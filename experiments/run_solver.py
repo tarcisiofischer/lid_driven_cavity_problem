759256f81c563848ad9aaa20e92096d000097598
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-08-27 02:04:08 -0300

    Update run_solver to use Ghia's tables for Re=0.01, 10, 100, 400 and 1000

--- experiments/run_solver.py
+++ experiments/run_solver.py
@@ -46,7 +46,7 @@ nx = 80
 ny = 80
 dt = 1e-2
 rho = 1.0
-final_time = 200.0
+final_time = None  # Run until steady state
 mi = 1.0
 Re = 10.0
 U_bc = (mi * Re) / (rho * size_x)
@@ -93,56 +93,43 @@ V = np.array(result.ns_y_mesh.phi)
 U = U.reshape(nx, ny - 1)
 V = V.reshape(nx - 1, ny)
 
+pos_ghia = np.loadtxt('ghia_ghia_shin_results/pos.txt')
+U_ghia = np.loadtxt('ghia_ghia_shin_results/U.txt')
+V_ghia = np.loadtxt('ghia_ghia_shin_results/V.txt')
+if np.isclose(Re, 0.01):
+    U_ghia = U_ghia[:, 0]
+    V_ghia = V_ghia[:, 0]
+elif np.isclose(Re, 10.0):
+    U_ghia = U_ghia[:, 1]
+    V_ghia = V_ghia[:, 1]
+elif np.isclose(Re, 100.0):
+    U_ghia = U_ghia[:, 2]
+    V_ghia = V_ghia[:, 2]
+elif np.isclose(Re, 400.0):
+    U_ghia = U_ghia[:, 3]
+    V_ghia = V_ghia[:, 3]
+elif np.isclose(Re, 1000.0):
+    U_ghia = U_ghia[:, 4]
+    V_ghia = V_ghia[:, 4]
+else:
+    print("Re=%s" % (Re,))
+    U_ghia = None
+    V_ghia = None
+
 plt.figure(3)
 plt.title("U velocity in the mesh center-x")
 U_normalized = U / U_bc
 U_center = U_normalized[:, len(U) // 2]
 plt.plot(U_center, np.linspace(0.0, size_y, len(U_center)))
-
-y_ghia = np.array([
-    0.0625,
-    0.125 ,
-    0.1875,
-    0.25  ,
-    0.3125,
-    0.375 ,
-    0.4375,
-    0.5   ,
-    0.5625,
-    0.625 ,
-    0.6875,
-    0.75  ,
-    0.8125,
-    0.875 ,
-    0.9375,
-])
-U_ghia = np.loadtxt('ghia_ghia_shin_results/re_10/U.txt')
-plt.plot(U_ghia, y_ghia, 'xb')
+if U_ghia is not None:
+    plt.plot(U_ghia, pos_ghia, 'xb')
 
 plt.figure(4)
 plt.title("V velocity in the mesh center-y")
 V_normalized = V / U_bc
 V_center = V_normalized[len(V) // 2, :]
 plt.plot(np.linspace(0.0, size_x, len(V_center)), V_center)
-
-x_ghia = np.array([
-    0.0625,
-    0.125 ,
-    0.1875,
-    0.25  ,
-    0.3125,
-    0.375 ,
-    0.4375,
-    0.5   ,
-    0.5625,
-    0.625 ,
-    0.6875,
-    0.75  ,
-    0.8125,
-    0.875 ,
-    0.9375,
-])
-V_ghia = np.loadtxt('ghia_ghia_shin_results/re_10/V.txt')
-plt.plot(x_ghia, V_ghia, 'xb')
+if V_ghia is not None:
+    plt.plot(pos_ghia, V_ghia, 'xb')
 
 plt.show()
