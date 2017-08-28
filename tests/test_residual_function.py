ffbcf0ba4aa6b3276577edacc99e5bd309727b7a
Tadeu Manoel <e.tadeu@gmail.com>
2017-08-28 20:23:57 -0300

    Add improvements to Cython code
    Also make import of C++ code lazy, as to not raise error when running other tests if it is not present

--- tests/test_residual_function.py
+++ tests/test_residual_function.py
@@ -1,7 +1,6 @@
 from lid_driven_cavity_problem.residual_function import pure_python_residual_function, \
     numba_residual_function, cython_residual_function, numpy_residual_function
 from lid_driven_cavity_problem.staggered_grid import Graph
-import _residual_function
 import numpy as np
 
 
@@ -31,11 +30,18 @@ def test_residual_function():
         initial_V=-0.01,
     )
 
-    reference_results = pure_python_residual_function.residual_function(x, graph)
-    for f in [
+    functions_to_try = [
         numpy_residual_function.residual_function,
-        _residual_function.residual_function,
         numba_residual_function.residual_function,
         cython_residual_function.residual_function,
-    ]:
+    ]
+
+    try:
+        import _residual_function
+        functions_to_try.append(_residual_function.residual_function)
+    except ImportError:
+        pass
+
+    reference_results = pure_python_residual_function.residual_function(x, graph)
+    for f in functions_to_try:
         assert np.allclose(f(x, graph), reference_results, rtol=1e-6, atol=1e-4)
