fdda840507eb4cbca34d7332d4890dc949635084
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2018-01-19 22:34:29 -0200

    Simplify tasks script and add cdt generator from CMake

--- tasks.py
+++ tasks.py
@@ -2,6 +2,25 @@ from invoke import task
 
 @task
 def compile(ctx):
-    ctx.run("python setup.py build_ext --inplace")
-    ctx.run("mkdir -p build")
-    ctx.run("cd build && cmake .. && make && make install")
+    commands = [
+        'python setup.py build_ext --inplace',
+        'mkdir -p build',
+        'cd build',
+        'cmake ..',
+        'make',
+        'make install'
+    ]
+    ctx.run(' && '.join(commands))
+
+
+@task
+def cdt(ctx, build_type='Debug', target='../lid_driven_cavity_problem_cdt'):
+    import os
+    current_dir = os.getcwd()
+
+    commands = [
+        'mkdir -p %s' % (target,),
+        'cd %s' % (target,),
+        'cmake %s -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=%s' % (current_dir, build_type,),
+    ]
+    ctx.run(' && '.join(commands))
